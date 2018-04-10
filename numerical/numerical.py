5496ac0d3ddb7537c6f56e601bd05de824240d71
Karan Mitroo <karan1.mitroo@gmail.com>
2018-04-10 15:25:15 +0530

    :heavy_check_mark: Numerical distance for two objects.

--- numerical/numerical.py
+++ numerical/numerical.py
@@ -1,13 +1,20 @@
 import numpy
 import random
-
+from math import sqrt
 def z_score_norm(num, mean, std_dev):
     return (num - mean) / std_dev
-    
+
+def n_dist(x, y):
+    total = 0
+    for i in range(len(z_scores[0])):
+        total += pow(abs(z_scores[x][i] - z_scores[y][i]), 2)
+        
+    return sqrt(total)
+        
 data = numpy.loadtxt("../datasets/contraceptive_method_choice.csv", delimiter=",")
 
 # Specify the number of columns containging numerical data in 0 index format
-numerical = data[:, [0,4]]
+numerical = data[:, [0,3]]
 mean = []
 std_dev = []
 
@@ -23,4 +30,4 @@ for i in range(0, len(numerical)):
     for j in range(len(numerical[i])):
         z_scores[i].append(z_score_norm(numerical[i][j], mean[j], std_dev[j]))
          
-print z_scores
+print n_dist(176, 298)
