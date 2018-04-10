c05235c3f6a5ab5561ec22bac444efeee1339999
Karan Mitroo <karan1.mitroo@gmail.com>
2018-04-10 14:14:59 +0530

    Calculating the normalized z-score values for numerical data

--- numerical/numerical.py
+++ numerical/numerical.py
@@ -1,10 +1,26 @@
 import numpy
 import random
-a = []
 
+def z_score_norm(num, mean, std_dev):
+    return (num - mean) / std_dev
+    
+data = numpy.loadtxt("../datasets/contraceptive_method_choice.csv", delimiter=",")
 
-for i in range(100):
-    a.append(random.randint(1,10))
-print a
-mean = numpy.mean(a)
-std_dev = numpy.std(a)
+# Specify the number of columns containging numerical data in 0 index format
+numerical = data[:, [0,4]]
+mean = []
+std_dev = []
+
+#Calculating the mean and the standard deviation for the numerical attributes
+for i in range(len(numerical[0])):
+    mean.append(numpy.mean(numerical[:, i]))
+    std_dev.append(numpy.std(numerical[:, i]))
+
+#Calculating the z_scores for all the numerical values.
+z_scores = []
+for i in range(0, len(numerical)):
+    z_scores.append([])
+    for j in range(len(numerical[i])):
+        z_scores[i].append(z_score_norm(numerical[i][j], mean[j], std_dev[j]))
+         
+print z_scores
