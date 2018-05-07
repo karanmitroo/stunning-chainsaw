c83e56c9b47ec815d6428ce4fa2dd5d256466eca
Karan Mitroo <karan1.mitroo@gmail.com>
2018-05-07 10:45:41 +0530

    Numerical data funtion improved.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# from utils.numerical import numerical_dist
+from utils.numerical import numerical_dist
 from utils.binning import binning
 import numpy
 
-# data = numpy.loadtxt("datasets/tae.csv", delimiter=",", usecols = (0,1,2,3))
-data = numpy.loadtxt("datasets/congressional_voting_records.csv", delimiter=",", dtype=str)
+data = numpy.loadtxt("datasets/stocks.csv", delimiter=",",dtype = str)
+# data = numpy.loadtxt("datasets/congressional_voting_records.csv", delimiter=",", dtype=str)
 
-categorical = data[:, :]
+categorical = data[:, (0,2,3)]
+numerical = data[:, (1, 4)].astype(numpy.float)
 k = 2
 
 categorical = binning(k, categorical)
 
 # print categorical
+# print numerical_dist(11, 12, numerical)
--- utils/numerical.py
+++ utils/numerical.py
@@ -5,30 +5,26 @@ from math import sqrt
 def z_score_norm(num, mean, std_dev):
     return (num - mean) / std_dev
 
-def numerical_dist(x, y):
+def numerical_dist(x, y, numerical):
+    mean = []
+    std_dev = []
+
+    #Calculating the mean and the standard deviation for the numerical attributes
+    for i in range(len(numerical[0])):
+        mean.append(numpy.mean(numerical[:, i]))
+        std_dev.append(numpy.std(numerical[:, i]))
+
+    #Calculating the z_scores for all the numerical values.
+    z_scores = []
+    for i in range(0, len(numerical)):
+        z_scores.append([])
+        for j in range(len(numerical[i])):
+            z_scores[i].append(z_score_norm(numerical[i][j], mean[j], std_dev[j]))
+
     total = 0
     for i in range(len(z_scores[0])):
         total += pow(abs(z_scores[x][i] - z_scores[y][i]), 2)
         
     return sqrt(total)
-        
-# data = numpy.loadtxt("datasets/contraceptive_method_choice.csv", delimiter=",")
-data = numpy.loadtxt("datasets/iris.csv", delimiter=",", usecols = (0,1,2,3))
 
-# Specify the number of columns containging numerical data in 0 index format
-numerical = data[:, 0:4]
-mean = []
-std_dev = []
-
-#Calculating the mean and the standard deviation for the numerical attributes
-for i in range(len(numerical[0])):
-    mean.append(numpy.mean(numerical[:, i]))
-    std_dev.append(numpy.std(numerical[:, i]))
-
-#Calculating the z_scores for all the numerical values.
-z_scores = []
-for i in range(0, len(numerical)):
-    z_scores.append([])
-    for j in range(len(numerical[i])):
-        z_scores[i].append(z_score_norm(numerical[i][j], mean[j], std_dev[j]))
 
